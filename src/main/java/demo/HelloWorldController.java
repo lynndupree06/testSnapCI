971151b6335b35d589864a054b2eb55c31e93f76
Jessica Brazelton <jbrazelt@thoughtworks.com>
2015-06-04 12:28:57 -0500

    first commit

---
+++ src/main/java/demo/HelloWorldController.java
@@ -0,0 +1,20 @@
+package demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloWorldController{
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String printHello(ModelMap model) {
+        model.addAttribute("message", "Hello Spring MVC Framework!");
+
+        return "hello";
+    }
+
+}
---
+++ src/main/java/demo/TestSnapCiApplication.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSnapCiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSnapCiApplication.class, args);
+    }
+}
